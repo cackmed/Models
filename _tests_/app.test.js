af6e15425c8204b861e8e9ed8b1af688cab96187
Caleb Pendergraft <cackmed@gmail.com>
2019-12-08 21:18:40 -0800

    started writiing out tests, dealt with error

--- _tests_/app.test.js
+++ _tests_/app.test.js
@@ -1,12 +1,36 @@
 const request = require('supertest');
 const app = require('../lib/app');
+const mongoose = require('mongoose');
+const Dog = require('../lib/models/Dog');
+require('../lib/utils/connect')();
 
-describe('application routes', () => {
-  it('has a home route that says hello everyone', () => {
+
+describe('testing curd routes', () => {
+  
+  beforeAll(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+  it('has a get route that retrives a dog by id form database', async() => {
+    const dog = await Dog.create({
+      name: 'Leo',
+      age: 3,
+      weight: '75'
+    });
+
     return request(app)
-      .get('/')
+      .get(`/dog/${dog._id}`)
       .then(res => {
-        expect(res.body).toEqual({ text: 'hello' });
+        expect(res.body).toEqual({ 
+          _id: expect.any(String),
+          __v: 0,
+          name: 'Leo',
+          age: 3,
+          weight: '75'
+        });
       });
   });
 
